bb2afcad1abdb5fa652ec2831a240fec17c7f913
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-13 20:00:33 +0700

    feat: float error

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 local o = vim.o
-o.guifont = "Iosevka Nerd Font:h12"
+-- o.guifont = "Iosevka Nerd Font:h12"
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,27 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local function au(typ, pattern, cmdOrFn)
+  if type(cmdOrFn) == "function" then
+    vim.api.nvim_create_autocmd(typ, { pattern = pattern, callback = cmdOrFn, group = group })
+  else
+    vim.api.nvim_create_autocmd(typ, { pattern = pattern, command = cmdOrFn, group = group })
+  end
+end
+
+au({ "CursorHold", "InsertLeave" }, nil, function()
+  local opts = {
+    focusable = false,
+    scope = "cursor",
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
+  }
+  vim.diagnostic.open_float(nil, opts)
+end)
+
+au("InsertEnter", nil, function()
+  vim.diagnostic.enable(false)
+end)
+
+au("InsertLeave", nil, function()
+  vim.diagnostic.enable(true)
+end)
