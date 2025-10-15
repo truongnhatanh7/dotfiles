880ec170311b916a36c0854400cb4b5d79cb23a3
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-15 18:50:33 +0700

    fix: scrollview gitsigns

--- lua/plugins/scrollview.lua
+++ lua/plugins/scrollview.lua
@@ -1,7 +1,14 @@
 return {
   "dstein64/nvim-scrollview",
-  opts = {
-    diagnostics_severities = { vim.diagnostic.severity.ERROR },
-    signs_on_startup = { "all" },
-  },
+  depedencies = { {
+    "lewis6991/gitsigns.nvim",
+  } },
+  config = function()
+    require("scrollview.contrib.gitsigns").setup({})
+    require("scrollview").setup({
+      diagnostics_severities = { vim.diagnostic.severity.ERROR },
+      scrollview_diagnostics_warn_symbol = "",
+      signs_on_startup = { "all" },
+    })
+  end,
 }
