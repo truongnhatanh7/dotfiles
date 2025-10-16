491688cef9e46a4977026a058560888b08ec3a87
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-16 10:59:08 +0700

    feat: config severity

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,27 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.lsp.set_log_level("off")
+
+vim.diagnostic.config({
+  virtual_text = {
+    severity = {
+      vim.diagnostic.severity.ERROR,
+      vim.diagnostic.severity.INFO,
+      vim.diagnostic.severity.HINT,
+    },
+  },
+  signs = {
+    severity = {
+      vim.diagnostic.severity.ERROR,
+      vim.diagnostic.severity.INFO,
+      vim.diagnostic.severity.HINT,
+    },
+  },
+  underline = {
+    severity = {
+      vim.diagnostic.severity.ERROR,
+      vim.diagnostic.severity.INFO,
+      vim.diagnostic.severity.HINT,
+    },
+  },
+})
