d2a6fc85f9817f1c60adc746f0553bfda44e9643
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-16 12:57:45 +0700

    feat: config neotest

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dev = true,
+    dependencies = {
+      "fredrikaverpil/neotest-golang",
+    },
+    opts = {
+      output = {
+        enabled = true,
+      },
+      output_panel = {
+        enabled = true,
+      },
+      adapters = {
+        ["neotest-golang"] = {
+          -- runner = "gotestsum",
+          warn_test_name_dupes = false,
+          warn_test_not_executed = false,
+        },
+      },
+    },
+  },
+}
