db4915309733bd0f8224656c41ba533ba4ac8c54
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-18 09:52:15 +0700

    feat: neotest discovery

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -5,6 +5,19 @@ return {
       "fredrikaverpil/neotest-golang",
     },
     opts = {
+      discovery = {
+        -- Drastically improve performance in ginormous projects by
+        -- only AST-parsing the currently opened buffer.
+        enabled = false,
+        -- Number of workers to parse files concurrently.
+        -- A value of 0 automatically assigns number based on CPU.
+        -- Set to 1 if experiencing lag.
+        -- concurrent = 1,
+      },
+      running = {
+        -- Run tests concurrently when an adapter provides multiple commands to run.
+        concurrent = true,
+      },
       output = {
         enabled = true,
       },
