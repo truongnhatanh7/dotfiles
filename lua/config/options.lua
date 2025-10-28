80e047f5a9ffc0c27180fd2e1714cd31b735f1e0
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-10-28 10:12:11 +0700

    feat: change cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.lsp.set_log_level("off")
+
+-- ~/.config/nvim/lua/config/options.lua
+-- This sets custom Neovim options
+local opt = vim.opt
+
+opt.guicursor = "a:blinkwait700-blinkoff250-blinkon250"
