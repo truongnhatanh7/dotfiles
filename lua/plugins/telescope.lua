8ba8d2c50e8a51d8dccc503fd7447b811de4e6a6
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-11-09 15:20:17 +0700

    fix: fzf nil

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -61,7 +61,12 @@ return {
         },
       },
       extensions = {
-        "fzf",
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
       },
     })
     telescope.load_extension("fzf")
