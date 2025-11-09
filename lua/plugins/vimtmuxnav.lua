f54de8c1d42150cb1454d20e15ad83ca550b2254
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-11-09 14:33:31 +0700

    feat: add vim tmux nav

---
+++ lua/plugins/vimtmuxnav.lua
@@ -0,0 +1,9 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", ":TmuxNavigateLeft<CR>", desc = "Navigate to left tmux pane" },
+    { "<C-j>", ":TmuxNavigateDown<CR>", desc = "Navigate to down tmux pane" },
+    { "<C-k>", ":TmuxNavigateUp<CR>", desc = "Navigate to up tmux pane" },
+    { "<C-l>", ":TmuxNavigateRight<CR>", desc = "Navigate to right tmux pane" },
+  },
+}
