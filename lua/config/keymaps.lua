6555ada33bf2befbd7a1a23dca8a1068907fe22a
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-11-13 18:26:36 +0700

    feat: float diag

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,5 @@
 vim.keymap.set("n", "<leader>sz", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
 vim.keymap.set("n", "<leader>W", ":wall<CR>", { desc = "Save all buffers" })
+
+vim.keymap.set("n", "<leader>ce", ":lua vim.diagnostic.open_float()<CR>", { desc = "Show line diagnostics" })
