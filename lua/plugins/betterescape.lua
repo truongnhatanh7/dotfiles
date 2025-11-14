c98725bd3a263c44e68458d108221e3593a17e7b
Truong Nhat Anh <truongnhatanhw@gmail.com>
2025-11-14 18:31:23 +0700

    feat: config better escape

--- lua/plugins/betterescape.lua
+++ lua/plugins/betterescape.lua
@@ -1,6 +1,38 @@
 return {
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup()
+    require("better_escape").setup({
+      timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
+      default_mappings = false, -- setting this to false removes all the default mappings
+      mappings = {
+        -- i for insert
+        i = {
+          j = {
+            -- These can all also be functions
+            k = "<Esc>",
+          },
+        },
+        c = {
+          j = {
+            k = "<C-c>",
+          },
+        },
+        t = {
+          j = {
+            k = "<C-\\><C-n>",
+          },
+        },
+        v = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+        s = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+      },
+    })
   end,
 }
